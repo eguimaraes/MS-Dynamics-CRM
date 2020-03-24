ae9098cd1e5907583629d1918daad5cdad36c09c
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-03-24 20:06:52 -0300

    Conexao usando classe
    classe

---
+++ ConectionClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConectionClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConectionClass")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8da83957-4813-4de1-8356-675bf35c15ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConectionClass/conexao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Discovery;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+
+
+namespace ConectionClass
+{
+    class Conexao
+    {
+        public Uri uri;
+        public ClientCredentials clientCredentials;
+        public OrganizationServiceProxy serviceProxy;
+
+        public Conexao(string u, string p,string uriStr)
+        {
+            clientCredentials.UserName.UserName = u;
+            clientCredentials.UserName.Password = p;
+            uri = new Uri(uriStr);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            serviceProxy = new OrganizationServiceProxy(uri, null, clientCredentials, null);
+            serviceProxy.EnableProxyTypes();
+        }
+
+
+    }
+}
