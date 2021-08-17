3367c34aeaeb99b4a823629fb35c4b230bd7e0cc
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-08-17 16:11:18 -0300

    Create getSessionUI.js
    https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/formcontext-ui-sections

---
+++ getSessionUI.js
@@ -0,0 +1,4 @@
+//https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/formcontext-ui-sections
+
+var tabObj = formContext.ui.tabs.get(arg);
+var sectionObj = tabObj.sections.get(arg);
